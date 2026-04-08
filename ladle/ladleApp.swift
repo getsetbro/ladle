78c7aab066a2a939f2c8fd509b9d92846ae7efab
Seth Broweleit <seth.browelit@ferguson.com>
2026-04-08 15:19:49 -0400

    Initial Commit

---
+++ ladle/ladleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ladleApp.swift
+//  ladle
+//
+//  Created by Seth Broweleit on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ladleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
